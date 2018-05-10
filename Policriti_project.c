a8c7fa52dbce5a4cb17f7be82c97e0f73a60bea4
PigiOfTheStone <pigi7dp@gmail.com>
2018-05-11 00:40:29 +0200

    Aggiunta lista di adiacenza

--- Policriti_project.c
+++ Policriti_project.c
@@ -1,26 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Struct BST
+typedef struct node NODE;
 struct node 
 {
     char key;
     struct node *left;
     struct node *right;
+    struct node *father;
 };
 
-typedef struct node NODE;
+//Struct Lista di adiacenza per grafo
+typedef struct list_node LIST_NODE;
+struct list_node
+{
+    struct node *next;
+    struct NODE *position; //Posizione della foglia nell'albero
+};
 
+//Prototipi
 NODE* bst_insert(NODE* n, char key);
 void bst_print(NODE* n);
 NODE* read_input();
 int max(int* counter);
 int min(int* counter);
+NODE* bst_search_for_leaf(NODE* root, char key);
+LIST_NODE* create_list();
+LIST_NODE* add_to_list(char key, LIST_NODE* last);
+
+//Variabili globali 
 int M;
 int m;
 
 int main(){
 
+    LIST_NODE* list = NULL; //Puntatore alla lista vuota delle adiacenze
     NODE* root = read_input();
+    bst_search_for_leaf(root, list);
+
     bst_print(root);
     //printf("\nMin: %d\nMax: %d\n", m, M);
 
@@ -70,7 +88,7 @@ NODE* read_input(){ //Legge la parola e lancia bst_insert per creare l'albero de
     return root;
 }
 
-int max(int* counter){
+int max(int* counter){ //Calcola il valore di M i.e. il numero massimo di occorrenze del carattere che si ripete di più nella stringa 
     int max_int = counter[0];
     for(int i = 0; i < 26; i++){
         if(counter[i] > max_int){
@@ -80,7 +98,7 @@ int max(int* counter){
     return max_int;
 }
 
-int min(int* counter){
+int min(int* counter){ //Calcola il valore di m i.e. il numero minimo di occorrenze del carattere che si ripete di meno nella stringa
     int min_int = counter[0];
     for(int i = 0; i < 26; i++){
         if(min_int == 0 && counter[i] != 0){
@@ -92,3 +110,42 @@ int min(int* counter){
     }
     return min_int;
 }
+
+LIST_NODE* bst_search_for_leaf(NODE* root, LIST_NODE* last){ //Cerca nel BST i nodi che sono foglia e chiama il creatore di lista di adiacenza
+    if(root != NULL){
+        if(root->left == NULL & root->right == NULL){
+            last = add_to_list(root, last);
+        }else{
+            last = bst_search_for_leaf(root->left, last);
+            last = bst_search_for_leaf(root->right, last);
+        }
+    }
+
+    return last;
+}
+
+LIST_NODE* add_to_list(NODE* leaf, LIST_NODE* last){
+    if(last == NULL){
+        last = (LIST_NODE*)malloc(sizeof(LIST_NODE));
+        last->position = leaf;
+        last->next = NULL;
+    }else{
+        last->next = (LIST_NODE*)malloc(sizeof(LIST_NODE));
+        last->next->position = leaf;
+        last->next->next = NULL;
+        last = last->next;
+    }
+    return last;
+}
+
+/* 
+if(last != NULL){
+    last = last->next;
+}
+
+last = malloc...
+last->next=null
+last->value=key;
+
+return last;
+ */
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## Log
 - 8 Maggio 2018: iniziato progetto, implementato BST e funzione di inserimento
 - 9 Maggio 2018: aggiunta funzione che legge la parola dallo standard input e che crea l'albero della parola
+- 10 Maggio 2018: aggiunte funzioni che cercano i nodi foglia nel BST, che inseriscono le foglie nella lista di adiacenza
